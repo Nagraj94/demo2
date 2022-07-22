b3c680f63bbdfdc440526c2c75bc09066c8cadef
Nagraj94 <ngowda335@gmail.com>
2022-07-22 17:25:51 +0530

    Added code for daily wages

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -4,10 +4,20 @@ public class EmpWageBuilder {
 
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HRS =20;
+
+        int empHrs;
+        int dailyWages;
         int empCheck = (int) (Math.floor(Math.random()*10)%2);
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("employee is present");
-        else
+            empHrs = 8;
+        }
+        else{
             System.out.println("employee is absent");
+            empHrs = 0;
+        }
+        dailyWages = empHrs*EMP_RATE_PER_HRS;
+        System.out.println("employee daily wages: "+dailyWages);
     }
 }
